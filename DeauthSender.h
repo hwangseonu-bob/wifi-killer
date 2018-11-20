27ffb4d9de8411a3723ada5681b110b19054cee9
hwangseonu <hwangseonu12@naver.com>
2018-11-20 09:05:16 +0900

    feat: Finish Deauth sender

--- DeauthSender.h
+++ DeauthSender.h
@@ -1,9 +1,21 @@
 #ifndef WIFI_KILLER_DEAUTHSENDER_H
 #define WIFI_KILLER_DEAUTHSENDER_H
 
+#include <iostream>
+#include <set>
+#include <tins/tins.h>
+
+using namespace std;
+using namespace Tins;
+
 class DeauthSender {
-
+private:
+    NetworkInterface interface;
+    HWAddress<6> target;
+    set<HWAddress<6>> *addressSet;
+public:
+    explicit DeauthSender(NetworkInterface interface, HWAddress<6> target, set<HWAddress<6>> *addressSet);
+    void sendPacket();
 };
-
 
 #endif //WIFI_KILLER_DEAUTHSENDER_H
